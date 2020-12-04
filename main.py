56f0bfca7fd7718a8c31ca1c1884ebd4e00605ff
_ara0n_ <thomas99.montero@gmail.com>
2020-12-04 21:44:45 +0100

    day 4 done

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ day2_part2 = lambda input=open("inputs/day2 input", "r"): len(["ok" for line in 
 # Day 3
 day3_part1 = lambda input=open("inputs/day3 input", "r"): len(["bonk!" for i, row in enumerate(input) if row.rstrip("\n")[3 * i % len(row.rstrip("\n"))] == "#"])
 day3_part2 = lambda input=open("inputs/day3 input", "r"): np.prod([len(["bonk!" for i, row in enumerate(input) if i%slope[1]==0 and row.rstrip("\n")[slope[0]*i//slope[1] % len(row.rstrip("\n"))] == "#"]) for slope in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]])
+
+# Day 4
+day4_part1 = lambda input=open("inputs/day4 input", "r"): len(["ok" for doc in input.read().split("\n\n") if re.match("^(?=.*byr)(?=.*iyr)(?=.*eyr)(?=.*hgt)(?=.*hcl)(?=.*ecl)(?=.*pid).*$", doc.replace("\n", " "))])
+day4_part2 = lambda input=open("inputs/day4 input", "r"): len(["ok" for doc in input.read().split("\n\n") if re.match("^(?=.*byr:(19[2-9]\d|200[0-2])( |$))(?=.*iyr:(201\d|2020)( |$))(?=.*eyr:(202\d|2030)( |$))(?=.*hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)( |$))(?=.*hcl:#[\da-f]{6}( |$))(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth)( |$))(?=.*pid:\d{9}( |$)).*$", doc.replace("\n", " "))])
