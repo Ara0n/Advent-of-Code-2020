4e65c841157cc917e57fdbbbb0c793c8b79e8366
_ara0n_ <thomas99.montero@gmail.com>
2020-12-21 17:03:55 +0100

    back from finals :D day 5 done

--- main.py
+++ main.py
@@ -16,3 +16,39 @@ day3_part2 = lambda input=open("inputs/day3 input", "r"): np.prod([len(["bonk!" 
 # Day 4
 day4_part1 = lambda input=open("inputs/day4 input", "r"): len(["ok" for doc in input.read().split("\n\n") if re.match("^(?=.*byr)(?=.*iyr)(?=.*eyr)(?=.*hgt)(?=.*hcl)(?=.*ecl)(?=.*pid).*$", doc.replace("\n", " "))])
 day4_part2 = lambda input=open("inputs/day4 input", "r"): len(["ok" for doc in input.read().split("\n\n") if re.match("^(?=.*byr:(19[2-9]\d|200[0-2])( |$))(?=.*iyr:(201\d|2020)( |$))(?=.*eyr:(202\d|2030)( |$))(?=.*hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)( |$))(?=.*hcl:#[\da-f]{6}( |$))(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth)( |$))(?=.*pid:\d{9}( |$)).*$", doc.replace("\n", " "))])
+
+
+def day5_long():
+    with open("inputs/day5 input", "r") as f:
+        data = f.readlines()
+    seat_id = []
+
+    for boardpass in data:
+        row = [i for i in range(128)]
+        seat = [i for i in range(8)]
+
+        for letter in boardpass:
+            if letter == "F":
+                row = row[:len(row)//2]
+            elif letter == "B":
+                row = row[len(row)//2:]
+            elif letter == "L":
+                seat = seat[:len(seat)//2]
+            elif letter == "R":
+                seat = seat[len(seat)//2:]
+
+        seat_id.append(row[0] * 8 + seat[0])
+
+    return seat_id
+
+def day5_part1_long():
+    return max(day5_long())
+
+def day5_part2_long():
+    boardpasses = day5_long()
+    valid_ids = [id for id in boardpasses if id > (0*8+7) and id < (127*8+0)]
+    valid_ids.sort()
+
+    for i in range(len(valid_ids)-1):
+        if valid_ids[i]+1 != valid_ids[i+1]:
+            return valid_ids[i]+1
